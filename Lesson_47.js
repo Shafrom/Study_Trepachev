0f16c064cf2b56ea2c40f8dd1080417aef87d241
Shafrom <shafrom@Shafroms-iMac.local>
2020-08-18 13:47:19 +0300

    add new lesson

---
+++ Lesson_47.js
@@ -0,0 +1,15 @@
+// Задача
+// Дан массив с элементами 'a', 'b', 'c', 'd', 'e'. Проверьте, что в этом массиве есть элемент 'c'. Если есть - выведите 'да', а если нет - выведите 'нет'.
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let flag = false;
+
+for(let elem of arr) {
+  if (elem === 'c') {
+    flag = true;
+    break;
+  }
+}
+
+if (flag === true) console.log('Да');
+  else console.log('Нет');
