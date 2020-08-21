2fb8586b8cf395dea430a3e32337aa67e3aeb607
Shafrom <shafrom@Shafroms-iMac.local>
2020-08-21 11:49:18 +0300

    add something

--- Lesson_56.js
+++ Lesson_56.js
@@ -45,3 +45,15 @@ for (let i = 1; i <= 10; i++) {
 }
 
 console.log(arr);
+
+// Задача 5
+// Дан массив с числами. Переберите его циклом и запишите в новый массив только положительные из чисел.
+
+let arr = [1, -2, 3, -4, 5, 6, -7, 16];
+let newArr = [];
+
+for (let elem of arr) {
+  if (elem >= 0) newArr.push(elem);
+}
+
+console.log(newArr);
