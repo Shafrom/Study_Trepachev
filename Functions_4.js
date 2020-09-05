65b1ab130c5c4e4a716cba32a5d80545bb134510
Shafrom <shafrom@Shafroms-iMac.local>
2020-09-05 14:40:27 +0300

    fix

--- Functions_4.js
+++ Functions_4.js
@@ -10,19 +10,152 @@ function getInt(arr1, arr2) {
 
 console.log(getInt([1, 2, 3, 4, 7], [2, 3, 4, 5, 7]));
 
-function getIntDiffer(arr1, arr2) {
+// Задача на разность елементов массивов
+
+function dif(arr1, arr2) {
+  return arr1.filter(i => !arr2.includes(i)).concat(arr2.filter(i => !arr1.includes(i)))
+}
+
+console.log(dif([1, 2, 3, 4, 7], [2, 3, 4, 5, 7]));
+
+// Еще более правильный вариант
+
+function diff(arr1, arr2) {
+	return arr1.concat(arr2).filter((el, i, array) => array.indexOf(el) === array.lastIndexOf(el))
+}
+
+console.log(diff([1, 2, 3, 4, 7], [2, 3, 4, 5, 7]));
+
+// Задача на наибольший общий делитель
+
+function divisors(num) {
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+function diff(arr1, arr2) {
+	return arr1.concat(arr2).filter((el, i, array) => array.indexOf(el) !== array.lastIndexOf(el))
+}
+
+const func = (num1, num2) => {
+	const arr = diff(divisors(num1), divisors(num2));
+  return arr.sort((a, b) => b - a)[0];
+//return Math.max(...arr); - либо вот так можно, используя оператор spread
+}
+
+console.log(func(120, 200));
+// --------------------------------
+
+function shuffle(arr) {
   let result = [];
-  //for (let elem of arr1) {
-    //if (arr2.filter((elem, arr1) => elem !== elem))
-    result = arr2.filter(function(elem, arr1) {
-      result.push(elem);
-    });
-  //}
+
+  while (arr.length > 0) {
+    let random = getRandomInt(0, arr.length - 1);
+    let elem = arr.splice(random, 1)[0];
+    result.push(elem);
+  }
   return result;
 }
 
-console.log(getIntDiffer([1, 2, 3, 4, 7], [2, 3, 4, 5, 7]));
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(shuffle([1, 2, 3, 4, 5]));
+
+// Задача
+// Сделайте функцию range принимающую параметрами 2 целых числа - начало и конец диапазона, и возвращающую массив, заполненный целыми числами из этого диапазона.
+
+function range(a, b) {
+  let arr = [];
+  for (let i = a; i <= b; i++) {
+     arr.push(i);
+  }
+  return arr;
+}
+
+console.log(range(1, 16));
+
+// Задача
+// Сделайте функцию rangeRand, возвращающую массив, заполненный случайными целыми числами из заданного диапазона. При этом числа не должны повторяться. Для решения задачи используйте комбинацию функций range и shuffle.
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function rangeRand(a, b) {
+  let arr = [];
+  for (let i = a; i <= b; i++) {
+     arr.push(i);
+  }
+  let res = [];
+  while (arr.length > 0) {
+    let random = getRandomInt(0, arr.length - 1);
+    let elem = arr.splice(random, 1)[0];
+    res.push(elem);
+   }
+   return res;
+}
+
+console.log(rangeRand(0, 8));
 
 
+
+// Задача на вывод счастливых 6-ти значных билетов в диапазоне от 000001 до 999999
+
+function getLuckyNumbers() {
+	let result = [];
+
+	for (let i = 1001; i <= 999999; i++) {
+		if (isLucky(i)) {
+      result.push(String(i));
+		}
+  }
+  return result;
+}
+function normalizeLuckyNumbers(getLuckyNumbers) {
+  let final = [];
+    for (let elem of getLuckyNumbers) {
+      if (elem.length === 5) {
+        elem = '0' + elem;
+        final.push(elem);
+      }
+      if (elem.length === 4) {
+        elem = '00' + elem;
+        final.push(elem);
+      }
+    }
+    return final;
+}
+
+
+function isLucky(num) {
+	let str = normalizeNum(num);
+
+	let sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
+	let sum2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
+
+	return sum1 == sum2;
+}
+
+function normalizeNum(num) {
+	let str = String(num);
+
+	if (str.length == 5) {
+		str = '0' + str;
+	}
+	if (str.length == 4) {
+		str = '00' + str;
+	}
+
+	return str;
+}
+
+console.log(normalizeLuckyNumbers(getLuckyNumbers()));
 
 
