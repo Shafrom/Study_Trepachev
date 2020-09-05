067e19f14235b15e5afe3058d82e388d4fafb74c
Shafrom <shafrom@Shafroms-iMac.local>
2020-09-05 15:18:27 +0300

    fix

--- Functions_4.js
+++ Functions_4.js
@@ -135,13 +135,13 @@ function isLucky(num) {
 	let str = normalizeNum(num);
 	let sum1 = Number(str[0]) + Number(str[1]) + Number(str[2]);
 	let sum2 = Number(str[3]) + Number(str[4]) + Number(str[5]);
-	return sum1 == sum2;
+	return sum1 === sum2;
 }
 
 function normalizeNum(num) {
 	let str = String(num);
-	if (str.length == 5) str = '0' + str;
-	if (str.length == 4) str = '00' + str;
+	if (str.length === 5) str = '0' + str;
+	if (str.length === 4) str = '00' + str;
 	return str;
 }
 
